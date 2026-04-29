25321c8680ee0072ea2a6a8ca8e15efa2335fd6a
sammmyd31 <sammypeter616@gmail.com>
2026-04-29 19:22:07 -0400

    Abstracted hittable objects

---
+++ src/sphere.hpp
@@ -0,0 +1,45 @@
+#ifndef SPHERE_H
+#define SPHERE_H
+
+#include "hittable.hpp"
+#include "vector3.hpp"
+
+class sphere : public hittable {
+public:
+    sphere(const point3& center, double radius) : center(center), radius(std::fmax(0, radius)) {}
+
+    bool hit(const ray& r, double ray_tmin, double ray_tmax, hit_record& rec) const override {
+        vector3 oc = center - r.origin();
+        double a = r.direction().length_squared();
+        double h = dot(r.direction(), oc);
+        double c = oc.length_squared() - radius*radius;
+
+        double discriminant = h*h - a*c;
+        if (discriminant < 0) {
+            return false;
+        }
+
+        double sqrtd = std::sqrt(discriminant);
+
+        double root = (h - sqrtd) / a;
+        if (root <= ray_tmin || ray_tmax <= root) {
+            root = (h + sqrtd) / a;
+            if (root <= ray_tmin || ray_tmax <= root) {
+                return false;
+            }
+        }
+
+        rec.t = root;
+        rec.p = r.at(rec.t);
+        vector3 outward_normal = (rec.p - center) / radius;
+        rec.set_face_normal(r, outward_normal);
+
+        return true;
+    }
+
+private:
+    point3 center;
+    double radius;
+};
+
+#endif
