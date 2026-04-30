4039933b3e29a8db59b6cb399efbbf1a96e25fb4
sammmyd31 <sammypeter616@gmail.com>
2026-04-30 18:06:33 -0400

    Fixed resolution

--- src/main.cpp
+++ src/main.cpp
@@ -52,8 +52,8 @@ int main() {
     camera cam;
 
     cam.aspect_ratio = 16.0 / 9.0;
-    cam.image_width = 100;
-    cam.samples_per_pixel = 500;
+    cam.image_width = 400;
+    cam.samples_per_pixel = 100;
     cam.max_depth = 50;
 
     cam.vfov = 20;
